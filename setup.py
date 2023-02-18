62c51bad4c2e3a92c0328a84b5e5885fa0a7830e
Amir <84547283+amir4v@users.noreply.github.com>
2023-02-18 20:43:04 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from io import open
 from setuptools import find_packages, setup
 
 CURRENT_PYTHON = sys.version_info[:2]
-REQUIRED_PYTHON = (3, 6)
+REQUIRED_PYTHON = (3, 5)
 
 # This check and everything above must remain compatible with Python 2.7.
 if CURRENT_PYTHON < REQUIRED_PYTHON:
@@ -16,17 +16,6 @@ if CURRENT_PYTHON < REQUIRED_PYTHON:
 ==========================
 Unsupported Python version
 ==========================
-This version of Django REST Framework requires Python {}.{}, but you're trying
-to install it on Python {}.{}.
-This may be because you are using a version of pip that doesn't
-understand the python_requires classifier. Make sure you
-have pip >= 9.0 and setuptools >= 24.2, then try again:
-    $ python -m pip install --upgrade pip setuptools
-    $ python -m pip install djangorestframework
-This will install the latest version of Django REST Framework which works on
-your version of Python. If you can't upgrade your pip (or Python), request
-an older version of Django REST Framework:
-    $ python -m pip install "djangorestframework<3.10"
 """.format(*(REQUIRED_PYTHON + CURRENT_PYTHON)))
     sys.exit(1)
 
@@ -37,26 +26,27 @@ version = '0.0.1'
 setup(
     name='action_throttle',
     version=version,
-    url='https://www.django-rest-framework.org/',
-    license='BSD',
-    description='Web APIs for Django, made easy.',
+    url='',
+    license='MIT',
+    description='Django Action Throttle',
     long_description=('README.md'),
     long_description_content_type='text/markdown',
-    author='Tom Christie',
-    author_email='tom@tomchristie.com',  # SEE NOTE BELOW (*)
+    author='Amirhosein Ghorbani',
+    author_email='amir4vx@gmail.com',
     packages=[],
     include_package_data=True,
     install_requires=[],
-    python_requires=">=3.6",
+    python_requires=">=3.5",
     zip_safe=False,
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 1 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
@@ -66,13 +56,5 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Internet :: WWW/HTTP',
     ],
-    project_urls={
-        
-    },
+    project_urls={},
 )
-
-# (*) Please direct queries to the discussion group, rather than to me directly
-#     Doing so helps ensure your question is helpful to other users.
-#     Queries directly to my email are likely to receive a canned response.
-#
-#     Many thanks for your understanding.
