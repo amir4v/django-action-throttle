d55e8debaff03d191cc5f54484c4a6877eca36bb
Amir <84547283+amir4v@users.noreply.github.com>
2023-05-23 16:59:12 +0330

    Update __init__.py

--- action_throttle/__init__.py
+++ action_throttle/__init__.py
@@ -3,10 +3,12 @@ Django Action Throttle
 """
 
 __title__ = 'Django Action Throttle'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'Amirhosein Ghorbani'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2022-'
 
 # Version synonym
 VERSION = __version__
+
+__all__ = ['admin', 'apps', 'middleware', 'models', 'rest', 'tests', 'throttle', 'throttle_settings', 'urls', 'utils', 'views',]
